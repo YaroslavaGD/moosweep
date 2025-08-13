7e9a904ead6ba204c18a8ac2ed738c82f7acbbe0
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-08-13 23:58:47 +0200

    feat: add mute button and numbers for volume

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,19 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AudioService } from '../../services/audio.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
 export class SettingsComponent implements OnInit {
   readonly audioService = inject(AudioService);
   volume!: number;
+  prevVolume!: number;
+  isOpen = false;
+  isMuted = false;
 
   ngOnInit() {
     this.volume = this.audioService.getGlobalVolume();
+    this.prevVolume = this.volume;
     console.log('volume =', this.volume);
   }
 
@@ -21,6 +26,24 @@ export class SettingsComponent implements OnInit {
     const input = event.target as HTMLInputElement;
     const newValue = input.valueAsNumber;
     this.volume = newValue;
+    this.prevVolume = newValue > 0 ? newValue : this.prevVolume;
+    this.isMuted = newValue === 0;
     this.audioService.setGlobalVolume(newValue);
+  }
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+
+    if (this.isMuted) {
+      this.audioService.setGlobalVolume(0);
+      this.volume = 0;
+    } else {
+      this.volume = this.prevVolume || 0.5;
+      this.audioService.setGlobalVolume(this.volume);
+    }
   }
 }
