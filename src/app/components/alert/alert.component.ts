83904d068ee9558e75a3c21c9728e5e13d212acd
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-08-30 15:35:51 +0200

    feat: add space/enter in alert-button

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,5 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  ElementRef,
+  SimpleChanges,
+  ViewChild,
+  HostListener,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -8,10 +19,47 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.scss',
 })
-export class AlertComponent {
+export class AlertComponent implements AfterViewInit, OnChanges {
   @Input() open = false;
   @Input() result: 'win' | 'lose' | null = null;
   @Output() closed = new EventEmitter<void>();
+
+  @ViewChild('closeBtn') closeBtn!: ElementRef<HTMLButtonElement>;
+
+  ngAfterViewInit(): void {
+    if (this.open) {
+      this.focusButton();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['open']?.currentValue) {
+      this.focusButton();
+    }
+  }
+
+  private focusButton() {
+    queueMicrotask(() => this.closeBtn?.nativeElement.focus());
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (!this.open) return;
+
+    if (event.code === 'Enter' || event.code === 'Space') {
+      event.preventDefault();
+
+      const btn = this.closeBtn?.nativeElement;
+      if (btn) {
+        btn.classList.add('pressed');
+
+        setTimeout(() => {
+          btn.classList.remove('pressed');
+          btn.click();
+        }, 150);
+      }
+    }
+  }
 
   onClose() {
     this.closed.emit();
